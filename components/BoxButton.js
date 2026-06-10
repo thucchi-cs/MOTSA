52fee8b40c48e33197dd8663be1519dcc86f9231
Thuc Chi Do <thucchi.code@gmail.com>
2026-06-10 00:46:59 -0500

    fix mobile home page

--- components/BoxButton.js
+++ components/BoxButton.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function BoxButton({ title, icon, link }) {
     return (
-        <a href={link} className="bg-zinc-200 w-70 flex-shrink-0 md:w-[23%] h-70 rounded-4xl text-white font-sans flex flex-col items-center justify-end pb-5 text-2xl border-10 border-zinc-400 transition-transform duration-300 hover:scale-105 cursor-pointer">
+        <a href={link} className="bg-zinc-200 w-65 md:w-70 flex-shrink-0 md:w-[23%] h-65 md:h-70 rounded-4xl text-white font-sans flex flex-col items-center justify-end pb-5 text-xl md:text-2xl border-10 border-zinc-400 transition-transform duration-300 hover:scale-105 cursor-pointer">
             <Image
             className=""
             src={icon}
@@ -10,7 +10,7 @@ export default function BoxButton({ title, icon, link }) {
             width={150}
             height={150}
             />
-            <h1 className="pt-5 text-red-500 font-bold">{title}</h1>
+            <h1 className="pt-5 text-red-500 text-center font-bold">{title}</h1>
         </a>
     );
 }
--- components/NewsSignUp.js
+++ components/NewsSignUp.js
@@ -1,20 +1,20 @@
 export default function NewsSignUp() {
     return (
         <div className="flex flex-col items-start justify-center w-full md:h-50 py-10 md:px-10">
-            <h1 className="font-bold text-lg md:text-3xl text-left">Subscribe to our newsletter</h1>
-            <h1 className="font-bold text-lg text-left pt-1"><p className="inline text-red-500 font-thin">*</p> Indicates required</h1>
+            <h1 className="font-bold text-xl md:text-3xl text-left">Subscribe to our newsletter</h1>
+            <h1 className="font-bold text-sm md:text-lg text-left pt-1"><p className="inline text-red-500 font-thin">*</p> Indicates required</h1>
             <form className="pt-4 w-full gap-y-4 flex flex-col">
                 <div className="flex flex-col items-start justify-center w-full">
-                    <h1 className="text-xl">Email Address <p className="inline text-red-500 font-thin">*</p></h1>
+                    <h1 className="text-md md:text-xl">Email Address <p className="inline text-red-500 font-thin">*</p></h1>
                     <input className="bg-white rounded-sm w-full border-3 border-zinc-400 h-10 text-black px-2" type="text"></input>
                 </div>
                 <div className="flex flex-row items-center justify-between gap-x-5">
                     <div className="flex flex-col items-start justify-center w-full">
-                        <h1 className="text-xl">First Name</h1>
+                        <h1 className="text-md md:text-xl">First Name</h1>
                         <input className="bg-white rounded-sm w-full border-3 border-zinc-400 h-10 text-black px-2" type="text"></input>
                     </div>
                     <div className="flex flex-col items-start justify-center w-full">
-                        <h1 className="text-xl">Last Name</h1>
+                        <h1 className="text-md md:text-xl">Last Name</h1>
                         <input className="bg-white rounded-sm w-full border-3 border-zinc-400 h-10 text-black px-2" type="text"></input>
                     </div>
                 </div>
