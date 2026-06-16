503ccbc521c7682e70a64e83eab1822e562b9bc2
Thuc Chi Do <thucchi.code@gmail.com>
2026-06-16 01:59:50 -0500

    redesign home page: mobile

--- components/News.js
+++ components/News.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function NewsBox({ label, link, content, date, img }) {
     return (
-        <a href={link} className="flex flex-col justify-start items-start gap-y-2 px-3 md:p-3 my-3 md:my-0 w-full h-full min-h-80 bg-white border-2 border-black transition-transform duration-300 hover:bg-zinc-200 text-black cursor-pointer">
+        <a href={link} className="flex flex-col justify-start items-start gap-y-2 p-3 md:p-3 my-3 md:my-0 w-[60vw] md:w-full h-full md:min-h-80 bg-white border-2 border-black transition-transform duration-300 hover:bg-zinc-200 text-black cursor-pointer">
             <Image
                 className="object-cover overflow-hidden h-[50%]"
                 src={ img }
@@ -11,9 +11,9 @@ export default function NewsBox({ label, link, content, date, img }) {
                 height={10}
                 priority
             />
-            <h1 className="font-bold text-md md:text-2xl underline text-left">{ label }</h1>
-            <p className="text-md md:text-md text-zinc-600 text-left">Posted: { date }</p>
-            <p className="text-md md:text-lg text-left">{ content }</p>
+            <h1 className="font-bold text-sm md:text-2xl underline text-left">{ label }</h1>
+            <p className="text-xs md:text-md text-zinc-600 text-left">Posted: { date }</p>
+            <p className="text-sm md:text-lg text-left">{ content }</p>
         </a>
     );
 }
