10d41fc315cc5b4e338385637b02be83bbe564db
Thuc Chi Do <thucchi.code@gmail.com>
2026-08-10 18:19:50 -0500

    add analytics

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NewsSignUp from "@/components/NewsSignUp";
 import Image from "next/image";
+import { Analytics } from '@vercel/analytics/next';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -53,6 +54,7 @@ export default function RootLayout({ children }) {
               </div>
           </div>
         </footer>
+        <Analytics />
       </body>
     </html>
   );
